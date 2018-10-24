067903a1f192a7dd90ec6cbddc87b7d7339487a5
Sky.Walker1998@yahoo.com <>
2018-10-24 15:44:36 -0400

    I love Sans Undertale

---
+++ src/forkthing/ForkThing.java
@@ -0,0 +1,24 @@
+/*
+ * Thing for Github Collab
+ */
+package forkthing;
+
+public class ForkThing {
+
+    public static void main(String[] args) {
+        System.out.println("Forking is neato!");
+        System.out.println("Oh so neato!");
+        System.out.println("IT'S SUPER NEATO!!!");
+System.out.println("░░░░░░░░░░░░░░░░░░░░░░░");
+System.out.println("░░░░░▄▄▀▀▀▀▀▀▀▀▀▄▄░░░░░");
+System.out.println("░░░░█░░░░░░░░░░░░░█░░░░");
+System.out.println("░░░█░░░░░░░░░░▄▄▄░░█░░░");
+System.out.println("░░░█░░▄▄▄░░▄░░███░░█░░░");
+System.out.println("░░░▄█░▄░░░▀▀▀░░░▄░█▄░░░");
+System.out.println("░░░█░░▀█▀█▀█▀█▀█▀░░█░░░");
+System.out.println("░░░▄██▄▄▀▀▀▀▀▀▀▄▄██▄░░░");
+System.out.println("░▄█░█▀▀█▀▀▀█▀▀▀█▀▀█░█▄░");
+System.out.println("░██░█▄▄█▄▄▄█▄▄▄█▄▄█░██░");
+        System.out.println("You're gonna have a bad time...");
+    }  
+}
